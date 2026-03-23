dfb5ba2cf2237ec3ba1609c0570dd6fd79cb5635
FXfire <fxfire195@gmail.com>
2026-03-23 19:59:29 +0900

    fix float to float3

--- Shader/normalImage.hlsl
+++ Shader/normalImage.hlsl
@@ -66,7 +66,7 @@ float4 PS(VS_OUTPUT input) : SV_Target
     float3 ambient = ambientColor.rgb;
     float3 direct = lightColorIntensity.rgb * (NdotL * lightColorIntensity.a);
 
-    float lighting = ambient + direct;
+    float3 lighting = ambient + direct;
     return float4(albedo.rgb * lighting, albedo.a);
 }
 
